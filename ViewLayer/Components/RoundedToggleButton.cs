e3175d86d3962083886a3d7819d1f2954da33074
Keishi Yamanae <apri.private.sign@gmail.com>
2025-08-28 01:19:56 +0900

    #7 RoundedToggleButton に ON/OFF 機能を追加
    `RoundedToggleButton` コントロールに ON/OFF 状態を管理する
    `Checked` プロパティを実装し、背景色やテキストが動的に
    変更されるようにしました。また、クリックイベントを処理
    することで、ユーザー操作による状態切り替えが可能です。

--- ViewLayer/Components/RoundedToggleButton.cs
+++ ViewLayer/Components/RoundedToggleButton.cs
@@ -1,20 +1,95 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace ViewLayer.Components
 {
 	public partial class RoundedToggleButton : RoundedButtonControl
 	{
-		public RoundedToggleButton ()
+		public RoundedToggleButton()
 		{
 			InitializeComponent();
+		}
+
+		private Color onBackColor = Color.FromArgb(50, 230, 80);  // 緑色 (ON:活性化)
+		private Color offBackColor = Color.FromArgb(189, 189, 189); // 灰色 (OFF)
+
+		private bool isChecked = false;
+		/// <summary>
+		/// チェック状態を取得または設定します
+		/// </summary>
+		/// 		[Browsable(true)]
+		[Category("Behavior")]
+		[Description("ラジオボタンの選択状態を設定します。")]
+		[DefaultValue(false)]
+		public bool Checked
+		{
+			get { return isChecked; }
+			set
+			{
+				if (isChecked != value)
+				{
+					isChecked = value;
+					Invalidate(); // 再描画
+				}
+
+				if (isChecked)
+				{
+					// 選択状態の場合、背景色をonBackColorに設定
+					BackgroundColor = onBackColor;
+					Text = onText;
+				}
+				else
+				{
+					// 未選択状態の場合、背景色を元のoffBackColorに設定
+					BackgroundColor = offBackColor;
+					Text = offText;
+				}
+			}
+		}
+
+		private string onText = "有効"; // ON時のテキスト
+		/// <summary>
+		/// ON状態の時に表示されるテキストを取得または設定します
+		/// </summary>
+		[Category("Appearance")]
+		[Description("ON状態の時に表示されるテキストを設定します")]
+		[DefaultValue("有効")]
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+		public string OnText
+		{
+			get { return onText; }
+			set
+			{
+				onText = value ?? string.Empty; // nullの場合は空文字列に変換
+				Invalidate(); // 画面を再描画
+			}
+		}
+
+		private string offText = "無効"; // OFF時のテキスト
+
+		/// <summary>
+		/// OFF状態の時に表示されるテキストを取得または設定します
+		/// </summary>
+		[Category("Appearance")]
+		[Description("OFF状態の時に表示されるテキストを設定します")]
+		[DefaultValue("無効")]
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+		public string OffText
+		{
+			get { return offText; }
+			set
+			{
+				offText = value ?? string.Empty; // nullの場合は空文字列に変換
+				Invalidate(); // 画面を再描画
+			}
+		}
+
+		private void RoundedToggleButton_Click(object sender, EventArgs e)
+		{
+			if (Enabled) // Enabledの場合のみ処理を実行
+			{
+				Checked = !Checked; // 状態を反転
+			}
 		}
 	}
 }
